b1caeea1ab6b5ae35e2c22da59b458c3d57ebcef
JuanPablo <jpabloaj@gmail.com>
2015-10-10 12:47:04 -0300

    agregada operaciones resta multi divicion

--- test/test_operations.js
+++ test/test_operations.js
@@ -6,6 +6,22 @@ describe('Operations', function(){
     assert.equal(5, operations.operation("+", [2, 3]));
   });
 
+  it('sumar dos numeros', function(){
+    assert.equal(-1, operations.operation("-", [2, 3]));
+  });
+
+  it('multiplicar dos numeros', function(){
+    assert.equal(6, operations.operation("*", [2, 3]));
+  });
+
+  it('dividir numeros', function(){
+    assert.equal(1.5, operations.operation("/", [3, 2]));
+  });
+
+  it('dividir por cero', function(){
+    assert.equal("null", operations.operation("/", [3, 0]));
+  });
+
   it('desconocido retorna null', function(){
     assert.equal("null", operations.operation('?', [1,2]))
   })
